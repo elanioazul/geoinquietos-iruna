b773ba49c0cebb272ac5dcabbd43da8ad11ba20d
hugo <hugo.herrador@gmail.com>
2024-10-12 23:48:25 +0200

    map db table to an entity

---
+++ back/src/entities/flota-municipal.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Geometry } from 'geojson';
+
+@Entity('puntos_flota_municipal_recarga_vehiculos_electricos')
+export class PuntosFlotaMunicipalRecargaVehiculosElectricos {
+  @PrimaryGeneratedColumn()
+  ogc_fid: number;
+
+  @Column('numeric', { nullable: true })
+  point_x: number;
+
+  @Column('numeric', { nullable: true })
+  point_y: number;
+
+  @Column('numeric', { nullable: true })
+  suma_conec: number;
+
+  @Column('varchar', { length: 255, nullable: true })
+  edificio: string;
+
+  @Column('varchar', { length: 255, nullable: true })
+  ubicacion_: string;
+
+  @Column('geometry', { nullable: true })
+  wkb_geometry: Geometry;
+}
